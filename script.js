7feda0503cb68b8b77684f6e78b4e9ed4692d7d3
devauntea <devauntea.thompson@csedge.com>
2019-02-16 11:54:43 -0800

    ok

---
+++ script.js
@@ -0,0 +1,43 @@
+//chalange0
+let playBtn = document.getElementById("play-button");
+
+playBtn.addEventListener("click", move);
+
+function move() {
+	playBtn.style.top = Math.random()*100 + "%";
+	playBtn.style.left = Math.random()*100 + "%";
+
+}
+
+let text = document.getElementById("challenge-one");
+
+text.addEventListener("dbclick", dis);
+ function dis()
+ {
+ 	text.style.remove(text)
+ }
+
+ let playBtn2 = document.getElementById("play-button");
+
+ playBtn2.addEventListener("mouseover",back)
+
+ function back() {
+ 	playBtn2.style.background = "black";
+
+ }
+
+ let copy = document.getElementById("page-title");
+
+ copy.addEventListener("copy", warn);
+
+ function warn (){
+ 	copy.innerHTML = "DO NOT COPY!";
+ }
+
+ let right = document.getElementById("body");
+
+ right.addEvenListener("contextmenu", fart)
+
+ function fart () {
+ 	right.style.background = url("images/pokemonBG2.png");
+ }
